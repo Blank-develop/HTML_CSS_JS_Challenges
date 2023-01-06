5dbb995050bb8d2248a4ae7111c67113d53671ce
Blank-develop <jilayouthbank@gmail.com>
2023-01-06 12:29:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+const scoreEl = document.getElementById("score")
+const inputEl = document.getElementById("input")
+const questionEl = document.getElementById("question")
+const formEl = document.getElementById("form")
+
+let score = JSON.parse(localStorage.getItem("score"))
+
+if(!score){
+    score = 0 
+}
+
+scoreEl.innerText = `score: ${score}`
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`
+
+const correctAns = num1 * num2
+
+
+
+
+formEl.addEventListener("submit",()=>{
+     const userAns = +inputEl.value
+     if(userAns == correctAns){
+       score ++
+       updateLocalStorage()
+     }else{
+        score --
+        updateLocalStorage()
+     }
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score))
+}
